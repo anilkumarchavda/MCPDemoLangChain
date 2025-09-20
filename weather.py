4e178ad58c3fadcddce65472d40cdc5d53e626a6
Anilkumar Chavda <anilchavda@hotmail.com>
2025-09-20 00:52:40 -0400

    Initial commit

---
+++ weather.py
@@ -0,0 +1,13 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp=FastMCP("Weather")
+
+@mcp.tool()
+async def get_weather(location:str)->str:
+    """ Get the weather location."""
+
+    return 'Its always raining in california'
+
+
+if __name__=='__main__':
+    mcp.run(transport="streamable-http")
